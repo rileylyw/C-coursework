3fd67812e5b23f7bd1a688a3b18b681fc1429db5
Riley Lee <uq21903@it075867.wks.bris.ac.uk>
2021-12-16 12:39:55 +0000

    update

--- NLab/nlab.h
+++ NLab/nlab.h
@@ -2,3 +2,45 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#include "general.c"
+
+#define BIGNUM 1000
+#define MAXNUMTOKENS 100
+#define MAXTOKENSIZE 20
+#define strsame(A,B) (strcmp(A, B)==0)
+#define ERROR(PHRASE) { fprintf(stderr, \
+          "Fatal Error %s occurred in %s, line %d\n", PHRASE, \
+          __FILE__, __LINE__); \
+          exit(EXIT_FAILURE); }
+#define CHARACTERS 26
+// #define DINTERP
+// #ifdef DINTERP
+// #endif
+
+struct var{
+//    char var[CHARACTERS]; //A-Z
+   char num[BIGNUM][BIGNUM];
+   int height;
+   int width;
+};
+typedef struct var var;
+
+struct prog{
+   char wds[MAXNUMTOKENS][MAXTOKENSIZE];
+   int cw; // Current Word
+   var* variable;
+};
+typedef struct prog Program;
+
+void Prog(Program *p);
+void Instrclist(Program *p);
+void test(void);
+// void Code(Program *p);
+// void Statement(Program *p);
+
+
+
+void test(void){
+    return;
+}
