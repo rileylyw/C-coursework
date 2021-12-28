393cb7936ab6983e87bca8d44f7e551335631914
djriles-hk <rileylyw@gmail.com>
2021-12-28 14:35:04 +0000

    parser testing

--- NLab/nlab.c
+++ NLab/nlab.c
@@ -18,8 +18,8 @@ bool verbose(char file[BIGNUM], int argc, char* argv[]){
       strcpy(file, argv[1]);
    }
    else if(argc==1){
-      fprintf(stderr, "No file");
-      // ERROR("No file");
+      // fprintf(stderr, "No file");
+      ERROR("No file");
    }
    return true;
 }
@@ -41,7 +41,9 @@ bool InstrcList(Program *p){
    if(strsame(p->wds[p->cw], "}")){
       return true;
    }
-   Instrc(p);
+   if(!Instrc(p)){
+      ERROR("Instuclist error");
+   }
    p->cw = p->cw + 1;
    InstrcList(p);
    return true;
@@ -50,20 +52,28 @@ bool InstrcList(Program *p){
 bool Instrc(Program *p){
    if(strsame(p->wds[p->cw], "PRINT")){
       // printf("Instrc %s\n", p->wds[p->cw]);
-      Print(p);
-      return true;
+      if(Print(p)){
+         return true;
+      };
+      ERROR("PRINT error");
    }
    if(strsame(p->wds[p->cw], "SET")){
-      Set(p);
-      return true;
+      if(Set(p)){
+         return true;
+      }
+      ERROR("SET error");
    }
    if(strsame(p->wds[p->cw], "ONES") || strsame(p->wds[p->cw], "READ")){
-      Create(p);
-      return true;
+      if(Create(p)){
+         return true;
+      }
+      ERROR("CREATE error");
    }
    if(strsame(p->wds[p->cw], "LOOP")){
-      Loop(p);
-      return true;
+      if(Loop(p)){
+         return true;
+      };
+      ERROR("LOOP error");
    }
    ERROR("Wrong instruction");
 }
@@ -84,7 +94,7 @@ bool Varname(Program *p){
    int len = strlen(p->wds[p->cw]);
    char* var = (char*)ncalloc(len+1, sizeof(char));
    strcpy(var, p->wds[p->cw]);  //TODO: if $ABC
-   printf("varname: %s\n", var);
+   // printf("varname: %s\n", var);
    if(!(var[0] == '$') || var[1]<'A' || var[1]>'Z'){
       free(var);
       return false;
@@ -122,6 +132,7 @@ bool Set(Program *p){
    p->cw = p->cw + 1; //$I
    // printf("p->wds[p->cw] %s\n", p->wds[p->cw]);
    if(!Varname(p)){
+      printf("set: %s\n", p->wds[p->cw]);
       ERROR("Wrong varname");
    }
    p->cw = p->cw + 1;
@@ -129,27 +140,47 @@ bool Set(Program *p){
       ERROR("Incorrect operator");
    }
    p->cw = p->cw + 1;
-   PolishList(p);
-   return true;
+   if(PolishList(p)){
+      return true;
+   }
+   return false;
+   // if(!PolishList(p)){
+   //    printf("%s\n", "Polishlist true");
+   //    ERROR("PolishList error");
+   // }
+   // printf("TESTING4 %s\n", p->wds[p->cw]);
+   // return true;
 }
 
 bool PolishList(Program *p){
-   // printf("begin %s\n", p->wds[p->cw]);
-   if(strsame(p->wds[p->cw], ";")){ //5
-      // printf("; %s\n", p->wds[p->cw]);
+   if(p->wds[p->cw] && !p->wds[p->cw][0]){
+      ERROR("NULL");
+   }
+   if(strsame(p->wds[p->cw], ";")){
+      printf("TESTING3 %s\n", p->wds[p->cw]);
       return true;
    }
    if(!Polish(p)){
+      // printf("%s\n", "TEST");
       ERROR("Polish error");
    }
    p->cw = p->cw + 1;
+   // printf("TESTING %d\n", p->wds[p->cw][0]);
+   // if(p->wds[p->cw] && !p->wds[p->cw][0]){
+   //    printf("TESTING2 %d\n", p->wds[p->cw][0]);
+   //    ERROR("NULL");
+   // }
    PolishList(p);
-   return false;
+   // printf("%s\n", "TEST");
+   if(!(strsame(p->wds[p->cw], ";"))){
+      ERROR("Missing ;");
+   }
+   return true;
 }
 
 bool Polish(Program *p){
    if(PushDown(p)){
-      printf("PushDown %s\n", p->wds[p->cw]);
+      // printf("PushDown %s\n", p->wds[p->cw]);
       return true;
    }
    else if(UnaryOp(p)){
@@ -165,11 +196,11 @@ bool Polish(Program *p){
 
 bool PushDown(Program *p){
    if(Varname(p)){
-      printf("varname %s\n", p->wds[p->cw]);
+      // printf("varname %s\n", p->wds[p->cw]);
       return true;
    }
    else if(Integer(p)){
-      printf("int %s\n", p->wds[p->cw]);
+      // printf("int %s\n", p->wds[p->cw]);
       return true;
    }
    return false;
@@ -228,20 +259,30 @@ bool BinaryOp(Program *p){
 bool Create(Program *p){
    if(strsame(p->wds[p->cw], "ONES")){
       p->cw = p->cw + 1;
-      printf("Rows %s\n", p->wds[p->cw]);
-      Rows(p);
+      // printf("Rows %s\n", p->wds[p->cw]);
+      if(!Rows(p)){
+         ERROR("ROWS error");
+      }
       p->cw = p->cw + 1;
-      printf("Cols %s\n", p->wds[p->cw]);
-      Cols(p);
+      // printf("Cols %s\n", p->wds[p->cw]);
+      if(!Cols(p)){
+         ERROR("COLS error");
+      }
       p->cw = p->cw + 1;
-      Varname(p);
+      if(!Varname(p)){
+         ERROR("Varname error");
+      }
       return true;
    }
-   else{
+   else if(strsame(p->wds[p->cw], "READ")){
       p->cw = p->cw + 1;
-      FileName(p);
+      if(!FileName(p)){
+         ERROR("FileName error");
+      }
       p->cw = p->cw + 1;
-      Varname(p);
+      if(!Varname(p)){
+         ERROR("Varname error");
+      }
       return true;
    }
    ERROR("Create error");
@@ -270,9 +311,13 @@ bool FileName(Program *p){
 
 bool Loop(Program *p){
    p->cw = p->cw + 1;
-   Varname(p);
+   if(!Varname(p)){
+      ERROR("Varname error");
+   }
    p->cw = p->cw + 1;
-   Integer(p);
+   if(!Integer(p)){
+      ERROR("Integer error");
+   }
    p->cw = p->cw + 1;
    if(!strsame(p->wds[p->cw], "{")){
       ERROR("Missing {");
--- NLab/test.c
+++ NLab/test.c
@@ -1,20 +1,145 @@
 #include "nlab.h"
 
-int main(int argc, char* argv[]){
-   Program* prog = ncalloc(1, sizeof(Program));
-   // char file[BIGNUM];
-   // verbose(file, argc, argv);
-   // readFile(file, prog);
-   strcpy(prog->wds[0], "BEGIN");
-   strcpy(prog->wds[1], "{");
-   strcpy(prog->wds[2], "{");
+void ExtractToken(char string[BIGNUM][BIGNUM], Program *p, int pos);
+void Reset(Program *p);
 
+int main(void){
+   Program* prog = ncalloc(1, sizeof(Program));
+
+   /* TEST PROG */
+   char string_prog[BIGNUM][BIGNUM] = {
+      {"BEGIN { }"}, //0: valid
+      {"BEGIN { SET $A := 0 ; }"}, //1: valid
+      {"{ SET $A := 0 ; }"}, //2: invalid, missing BEGIN
+      {"BEGIN SET $A := 0 ; }"} //3: invalid, missing {
+   };
+
+   ExtractToken(string_prog, prog, 0); //store string to prog
    assert(Prog(prog));
-   // printf("b %s\n", prog->wds[1]);
-   // printf("%s\n", prog->wds[prog->cw]);
-   assert(!Instrclist(prog));
+   Reset(prog);
+
+   ExtractToken(string_prog, prog, 1);
+   assert(Prog(prog));
+   Reset(prog);
+
+   ExtractToken(string_prog, prog, 2);
+   assert(!Prog(prog));
+   Reset(prog);
+
+   ExtractToken(string_prog, prog, 3);
+   assert(!Prog(prog));
+   Reset(prog);
+
+   /* TEST INSTRCLIST */
+   char string_instrclist[BIGNUM][BIGNUM] = {
+      {"}"}, //0: valid
+      {"PRINT $A }"}, //1: valid
+      {"{ SET $A := 0 ; }"}, //2: invalid, has {
+      {"BEGIN SET $A := 0 ; }"} //3: invalid, has BEGIN
+   };
+   ExtractToken(string_instrclist, prog, 0);
+   assert(InstrcList(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrclist, prog, 1);
+   assert(InstrcList(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrclist, prog, 2);
+   assert(!InstrcList(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrclist, prog, 3);
+   assert(!InstrcList(prog));
+   Reset(prog);
+   // printf("test prog->wds[0]: %s\n", prog->wds[0]);
+
+   /* TEST INSTRC */
+   char string_instrc[BIGNUM][BIGNUM] = {
+      {"PRINT $A"}, //0: valid
+      {"SET $Z := 1000 ;"}, //1: valid
+      {"SET $X := $Y ;"}, //2: valid
+      {"READ \"test.arr\" $A"}, //3: valid
+      {"READ"}, //4: invalid
+      {"SET $X = $Y"}, //5: invalid, no :=
+      {"LOOP $I 999 {"}, //6: valid
+      {"LOOP $I 999"}, //7: invalid, missing {
+      {"LOOP $I A12 {"}, //8: invalid, not int
+      {"ONES 10 10 $A"}, //9: valid
+      {"ONES"}, //10: invalid
+      {"ONES 1 $Z"}, //11: invalid
+      {"ONES $I"}, //12: invalid
+   };
+   ExtractToken(string_instrc, prog, 0);
+   assert(Instrc(prog));
+   Reset(prog);
+   // printf("prog->cw: %d\n", prog->cw);
+   // printf("prog->wds[0]: %s\n", prog->wds[0]);
+
+   ExtractToken(string_instrc, prog, 1);
+   assert(Instrc(prog));
+   Reset(prog); 
+
+   ExtractToken(string_instrc, prog, 2);
+   assert(Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 3);
+   assert(Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 4);
+   assert(!Instrc(prog));
+   Reset(prog);
+   
+   ExtractToken(string_instrc, prog, 5);
+   assert(!Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 6);
+   assert(Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 7);
+   assert(!Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 8);
+   assert(!Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 9);
+   assert(Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 10);
+   assert(!Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 11);
+   assert(!Instrc(prog));
+   Reset(prog);
+
+   ExtractToken(string_instrc, prog, 12);
+   assert(!Instrc(prog));
+   Reset(prog);
 
    free(prog);
 
    return EXIT_SUCCESS;
+}
+
+void ExtractToken(char string[BIGNUM][BIGNUM], Program *p, int pos){
+   char* token = strtok(string[pos], " ");
+   int i = 0;
+   while(token != NULL){
+      strcpy(p->wds[i++], token);
+      // printf("%s\n", token); //printing each token
+      token = strtok(NULL, " ");
+   }
+}
+
+void Reset(Program *p){
+   memset(p->wds, 0, sizeof(p->wds));
+   p->cw = 0;
 }
